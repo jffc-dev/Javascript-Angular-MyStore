693305da2da1cf624b4446fdeacc8abe1cd090ad
Javier Flores <jffc.dev@gmail.com>
2022-06-28 22:11:14 -0500

    feat(knowledge): Property biding examples added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,14 @@ export class AppComponent {
   public name:string = 'Javier';
   public age:number = 20;
 
-  img:string = 'http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcR-2yegwDBmZY7_4LpHbuEDMBK5p1AZ6m42534LvT2Y-llG62ENo3gNmyoF0gNLgJD_'
+  img:string = 'http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcR-2yegwDBmZY7_4LpHbuEDMBK5p1AZ6m42534LvT2Y-llG62ENo3gNmyoF0gNLgJD_';
+
+  btnDisabled:boolean = false;
+  initialInput:string = 'Javier Flores';
+
+  person = {
+    name : "Javier",
+    age: 20,
+    img: 'https://netstorage-sportsbrief.akamaized.net/images/7325ee45ef6e709c.jpg?imwidth=900'
+  }
 }
