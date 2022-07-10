5cfeaa2af5f97563b812e3d5f62cb23989f9a233
Javier Flores <jffc.dev@gmail.com>
2022-07-10 16:45:32 -0500

    feat(knowledge): Styles grid for list of products added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Product } from './product.model'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass','./app.component.scss']
 })
 export class AppComponent {
   // public is the dafault access
